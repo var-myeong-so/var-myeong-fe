86692377aa99d9fb6511fe7662c113a826eea315
Jaeheon-So <sjh-98@naver.com>
2022-06-17 01:47:22 +0900

    Add loading spinner

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import SearchHeader from "../components/SearchHeader";
 import SearchMainResult from "../components/SearchMainResult";
 import SearchSideLayout from "../components/SearchSideLayout";
+import Spinner from "react-bootstrap/Spinner";
 
 const Search = () => {
-  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
   const q = searchParams.get("q");
   const filter = searchParams.get("filter");
@@ -18,7 +18,7 @@ const Search = () => {
   const [wordCount, setWordCount] = useState(0);
   const [variableCount, setVariableCount] = useState(0);
   const [lastCount, setLastCount] = useState(10);
-
+  const [loading, setLoading] = useState(false);
   const handleInputChange = (e) => {
     setSearchInput(e.target.value);
   };
@@ -38,6 +38,7 @@ const Search = () => {
   };
 
   const getData = async () => {
+    setLoading(true);
     const res = await fetch(url).then((res) => res.json());
     const classCountRes = await fetch(temp2 + `/code/count/class/${q}`).then(
       (classCountRes) => classCountRes.text()
@@ -52,6 +53,7 @@ const Search = () => {
     setVariableCount(variableCountRes);
     setWordCount(wordCountRes);
     setData(res);
+    setLoading(false);
   };
 
   const changeUrl = (newUrl) => {
@@ -102,33 +104,46 @@ const Search = () => {
             changeTotalCount={changeTotalCount}
           />
           <div className="search-main-layout">
-            <div className="search-main-header">
-              {data.length === 0 ? (
-                <></>
-              ) : (
-                <div className="search-page-info">
-                  Showing <strong>1 - {lastCount}</strong> out of{" "}
-                  <strong>{totalCount}</strong> results
+            {loading === true ? (
+              <Spinner
+                className="search-spinner"
+                animation="border"
+                role="status"
+                variant="primary"
+              >
+                <span className="visually-hidden">Loading...</span>
+              </Spinner>
+            ) : (
+              <>
+                <div className="search-main-header">
+                  {data.length === 0 ? (
+                    <></>
+                  ) : (
+                    <div className="search-page-info">
+                      Showing <strong>1 - {lastCount}</strong> out of{" "}
+                      <strong>{totalCount}</strong> results
+                    </div>
+                  )}
                 </div>
-              )}
-            </div>
-            <div className="search-main-body">
-              <ul className="search-result-container">
-                {data.length === 0 ? (
-                  <div className="search-no-result">
-                    We didn't find any matching results.
-                  </div>
-                ) : (
-                  data.map((it) => (
-                    <SearchMainResult
-                      key={it.id}
-                      itemList={it}
-                      searchWord={q}
-                    />
-                  ))
-                )}
-              </ul>
-            </div>
+                <div className="search-main-body">
+                  <ul className="search-result-container">
+                    {data.length === 0 ? (
+                      <div className="search-no-result">
+                        We didn't find any matching results.
+                      </div>
+                    ) : (
+                      data.map((it) => (
+                        <SearchMainResult
+                          key={it.id}
+                          itemList={it}
+                          searchWord={q}
+                        />
+                      ))
+                    )}
+                  </ul>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
